76f071c34e6a62eb07008b57158ac6db5ad32c92
RadoRado <radorado@hackbulgaria.com>
2015-05-08 13:02:34 +0300

    Add get_movie_by_id

--- cinema_db.py
+++ cinema_db.py
@@ -5,6 +5,19 @@ class CinemaDatabaseManager:
         FROM Movies
     """
 
+    GET_MOVIE_BY_ID_QUERY = """
+        SELECT movie_id, movie_name, rating
+        FROM Movies
+        WHERE movie_id = ?
+    """
+
+    @classmethod
+    def get_movie_by_id(cls, conn, movie_id):
+        cursor = conn.cursor()
+        result = cursor.execute(cls.GET_MOVIE_BY_ID_QUERY, (movie_id, ))
+
+        return result.fetchone()
+
 
     @classmethod
     def get_all_movies(cls, conn):
