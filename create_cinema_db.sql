ecac03c41f9787a8b70554d2f0236ccdbb34dd76
RadoRado <radorado@hackbulgaria.com>
2015-05-08 12:48:38 +0300

    Add initial test data for Movies table

--- create_cinema_db.sql
+++ create_cinema_db.sql
@@ -5,3 +5,9 @@ CREATE TABLE Movies(
   movie_name TEXT,
   movie_rating REAL
 );
+
+
+INSERT INTO Movies(movie_name, movie_rating)
+VALUES ("The Hunger Games: Catching Fire", 7.9),
+       ("Wreck-It Ralph", 7.8),
+       ("Her", 8.3);
